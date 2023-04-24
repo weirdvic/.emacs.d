543a0b8dcc1afd923bbc3284b9c033ab72f1a77b
weirdvic <56339132+weirdvic@users.noreply.github.com>
2023-04-24 20:18:06 +0300

    Настройки org-roam, светлая тема, мелкие настройки

--- custom.el
+++ custom.el
@@ -3,17 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "37768a79b479684b0756dec7c0fc7652082910c37d8863c35b702db3f16000f8" default))
  '(ispell-dictionary nil)
- '(org-babel-load-languages '((emacs-lisp . t) (python . t) (shell . t) (sql . t)))
  '(package-selected-packages
-   '(k8s-mode pyvenv which-key vterm ox-pandoc magit doom-themes php-mode circe crontab-mode zygospore su reverse-im rainbow-delimiters mood-line lsp-ui lsp-mode go-mode eyebrowse exec-path-from-shell dockerfile-mode docker-compose-mode company-ansible company ansible-vault ansible-doc ansible use-package))
- '(vterm-buffer-name-string "%s vterm"))
+   '(solo-jazz-theme consult-org-roam consult-ag org-roam company magit kubedoc kele solaire-mode pyvenv-auto treemacs treemacs-all-the-icons treemacs-magit treemacs-projectile zygospore which-key vterm use-package su reverse-im rainbow-delimiters pyvenv projectile php-mode ox-pandoc mood-line lsp-ui go-mode eyebrowse exec-path-from-shell dockerfile-mode docker-compose-mode crontab-mode circe))
+ '(tool-bar-mode nil)
+ '(vterm-buffer-name-string "%s vterm")
+ '(warning-suppress-log-types '(((unlock-file)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Cascadia Mono PL" :foundry "    " :slant normal :weight normal :height 110 :width normal))))
+ '(default ((t (:family "Fira Code" :foundry "CTDB" :slant normal :weight semi-bold :height 108 :width normal))))
  '(multi-magit-repo-heading ((t (:inherit magit-section-heading :box nil)))))
--- init.el
+++ init.el
@@ -30,7 +30,7 @@
 
 ;; Отдельные настройки для GUI версии
 (when (window-system)
-  (menu-bar-mode -1)
+  ;;(menu-bar-mode -1)
   (tool-bar-mode -1)
   (scroll-bar-mode -1)
   (setq-default frame-title-format '("Emacs " emacs-version)))
@@ -60,10 +60,45 @@
 ;; Переносить по словам
 (setq word-wrap t)
 (add-hook 'text-mode-hook 'turn-on-visual-line-mode)
-;; Отображать номера строк в буферах с исходниками
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
 (show-paren-mode t)
 (electric-pair-mode t)
+;; Настройки Dired
+(put 'dired-find-alternate-file 'disabled nil)
+(defvar my/dired-preview-temp-buffer nil)
+
+;;;###autoload
+(defun my/dired-preview ()
+  "Preview a file/directory at point in Dired.
+It's similar to `dired-display-file' but it checks if a buffer
+visiting a file is live.  If not, it will be put into a temp file
+to be removed."
+  (when my/dired-preview-temp-buffer (kill-buffer my/dired-preview-temp-buffer))
+  (setq my/dired-preview-temp-buffer nil)
+  (let* ((file (dired-get-file-for-visit))
+         (buf (find-buffer-visiting file)))
+    (unless buf
+      (setq buf (find-file-noselect file))
+      (setq my/dired-preview-temp-buffer buf))
+    ;; Check if a buffer visiting file is live; if not it's a temp buffer to be
+    ;; deleted.
+    (display-buffer buf t)))
+
+;;;###autoload
+(defun my/dired-next-line-display ()
+  (interactive)(dired-next-line 1)(my/dired-preview))
+
+;;;###autoload
+(defun my/dired-previous-line-display ()
+  (interactive)(dired-previous-line 1)(my/dired-preview))
+
+(add-hook 'dired-mode-hook
+          (lambda ()
+            (dired-hide-details-mode 1)
+            ;; I don't use dired-subtree at the moment
+            ;;(define-key dired-mode-map (kbd "<tab>") #'dired-subtree-toggle)
+            ;;(define-key dired-mode-map (kbd "<C-tab>") #'dired-subtree-cycle)
+            (define-key dired-mode-map (kbd "<down>") #'my/dired-next-line-display)
+            (define-key dired-mode-map (kbd "<up>") #'my/dired-previous-line-display)))
 ;; Дни недели и месяцы на русском языке
 (setq calendar-week-start-day 1
       calendar-day-name-array ["Вс" "Пн" "Вт" "Ср" "Чт" "Пт" "Сб"]
@@ -85,13 +120,17 @@
 ;; Прокрутка по одной линии за раз
 (setq scroll-step 1)
 ;; Настройки org-mode
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((emacs-lisp . t)
-   (python . t)
-   (shell . t)
-   (sql . t)
-   ))
+(setq org-babel-load-languages
+  '((emacs-lisp . t)
+    (python . t)
+    (shell . t)
+    (sql . t)))
+(setq org-todo-keywords
+  '((sequence "TODO" "WORK" "DONE")))
+(setq org-edit-src-content-indentation 0
+  org-adapt-indentation nil
+  org-src-tab-acts-natively t
+  yaml-indent-offset 4)
 
 ;; #############################################################################
 ;; #                                                                           #
@@ -99,13 +138,8 @@
 ;; #                                                                           #
 ;; #############################################################################
 
-;; Настройки для работы с Ansible
-(use-package ansible)
-(use-package ansible-doc)
-(use-package ansible-vault)
-
 ;; Управление буферами и список буферов по C-x C-b
-(use-package bs)
+
 (use-package ibuffer
   :config
   (defalias 'list-buffers 'ibuffer))
@@ -133,15 +167,13 @@
 (defun my-circe-set-margin ()
   (setq right-margin-width 5)))
 
-;; Настройки Company
+;; Настройки company-mode
 (use-package company
   :config
-  (setq company-idle-delay 0)
+  (setq company-idle-delay 0.05)
   (setq company-minimum-prefix-length 1)
-  (add-to-list 'company-backends 'company-ansible)
   (add-hook 'after-init-hook 'global-company-mode)
   (global-set-key (kbd "C-<tab>") 'company-complete))
-(use-package company-ansible)
 
 ;; Улучшенная работа с crontab файлами
 (use-package crontab-mode)
@@ -151,13 +183,12 @@
 (use-package dockerfile-mode)
 
 ;; Цветовые схемы
-(use-package doom-themes
+(use-package solo-jazz-theme
   :config
-  (setq doom-themes-enable-bold t
-        doom-themes-enable-italic t)
-  (load-theme 'doom-nord t)
-  (doom-themes-visual-bell-config)
-  (doom-themes-org-config))
+  (load-theme 'solo-jazz t))
+(use-package solaire-mode
+  :after (solo-jazz-theme)
+  :config (solaire-global-mode +1))
 
 ;; Включаем прозрачное шифрование файлов при помощи GPG
 ;; В файле secrets.el.gpg хранятся логины и пароли, которые нельзя хранить в
@@ -197,11 +228,12 @@
   (setq ido-virtual-buffers t)
   (setq ido-enable-flex-matching t))
 
-;; Kubernetes mode
-(use-package k8s-mode)
-
-;; Настройки lsp-mode для python и go
-;; https://emacs-lsp.github.io/lsp-mode/page/installation/
+;; Настройки интеграции с Kubernetes
+(use-package kele
+  :config
+  (kele-mode 1)
+  (bind-key (kbd "s-k") kele-command-map kele-mode-map))
+(use-package kubedoc)
 
 ;; Префикс для lsp-command-keymap
 (setq lsp-keymap-prefix "C-c l")
@@ -215,7 +247,7 @@
          (lsp-mode . lsp-enable-which-key-integration))
   :config
   (setq lsp-prefer-flymake nil
-         lsp-eldoc-hook nil)
+        lsp-eldoc-hook nil)
   :commands lsp)
 
 ;; Дополнительно
@@ -241,6 +273,60 @@
   :config
   (mood-line-mode))
 
+;; Настройки org-roam
+(use-package org-roam
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory "~/org-roam")
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n t" . org-roam-tag-add)
+         ("C-c n i" . org-roam-node-insert)
+         :map org-mode-map
+         ("C-M-i" . completion-at-point)
+         :map org-roam-dailies-map
+         ("Y" . org-roam-dailies-capture-yesterday)
+         ("T" . org-roam-dailies-capture-tomorrow))
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
+  :config
+  (require 'org-roam-dailies) ;; Ensure the keymap is available
+  (org-roam-db-autosync-mode)
+  (setq org-roam-node-display-template
+      (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag))))
+
+(use-package consult-ag)
+
+(use-package consult-org-roam
+   :after org-roam
+   :init
+   (require
+    'consult-org-roam
+    'consult-ag)
+   ;; Activate the minor mode
+   (consult-org-roam-mode 1)
+   :custom
+   ;; Use `ag' for searching with `consult-org-roam-search'
+   (consult-org-roam-grep-func #'consult-ag)
+   ;; Configure a custom narrow key for `consult-buffer'
+   (consult-org-roam-buffer-narrow-key ?r)
+   ;; Display org-roam buffers right after non-org-roam buffers
+   ;; in consult-buffer (and not down at the bottom)
+   (consult-org-roam-buffer-after-buffers t)
+   :config
+   ;; Eventually suppress previewing for certain functions
+   (consult-customize
+    consult-org-roam-forward-links
+    :preview-key (kbd "M-."))
+   :bind
+   ;; Define some convenient keybindings as an addition
+   ("C-c n e" . consult-org-roam-file-find)
+   ("C-c n b" . consult-org-roam-backlinks)
+   ("C-c n l" . consult-org-roam-forward-links)
+   ("C-c n r" . consult-org-roam-search))
+
 ;; Пакет для экспорта из .org в другие форматы
 (use-package ox-pandoc)
 
@@ -251,8 +337,17 @@
 
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 
+;; Управление проектами
+(use-package projectile
+  :init
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+              ("s-p" . projectile-command-map)
+              ("C-c p" . projectile-command-map)))
+
 ;; Поддержка виртуальных окружений Python
 (use-package pyvenv)
+(use-package pyvenv-auto)
 
 ;; Подсветка скобок
 (use-package rainbow-delimiters
@@ -271,10 +366,20 @@
   :config
   (su-mode +1))
 
+;; Пакеты treemacs для отображения файлового дерева
+(use-package treemacs
+  :bind ("<f9>" . treemacs)
+  :config (treemacs-follow-mode 1))
+(use-package treemacs-all-the-icons
+  :after (treemacs))
+(use-package treemacs-magit
+  :after (treemacs magit))
+(use-package treemacs-projectile
+  :after (treemacs projectile))
+
 ;; Пакет vterm для эмулятора терминала внутри Emacs
 (use-package vterm
-  :bind
-  ("s-x" . vterm)
+  :bind ("s-x" . vterm)
   :config
   (setq vterm-kill-buffer-on-exit t)
   (setq vterm-buffer-name-string "%s vterm"))
